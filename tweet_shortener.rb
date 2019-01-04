d69cc0c6a0d563e08ca4feac9c34cf13c54fc0dd
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 03:27:24 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -28,7 +28,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(bulk)
-  array = bulk.split
+  array = bulk.inspect
   subs = dictionary.keys
     array.each_with_index do |word, i|
       subs.each do |sub|
