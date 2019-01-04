9ee4619586cee30b982c966e54ce69ba00b7691d
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 03:16:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,8 +21,7 @@ def word_substituter(tweet)
 
   array = tweet.split
   subs = dictionary.keys
-  narray = array
-    narray.each do |word|
+    array.each do |word|
     subs.each do |sub|
       if word == sub
         word = dictionary.values_at(sub)
