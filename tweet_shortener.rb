4cef0306a24b8c1c94fc465848a37220aafc6ee4
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 02:34:12 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,18 +3,17 @@
 def dictionary
 
 dictionary = {
-  "hello" => "hi", 
-  "to" => "2", 
-  "two" => "2", 
-  "too" => "2", 
-  "for" => "4", 
-  "four" => "4", 
-  "be" => "b", 
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
   "you" => "u"
   "at" => "@"
   "and" => "&"  
  }
-  
 end
 
 def word_substituter
